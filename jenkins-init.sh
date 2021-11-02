ef8bebf18f0f2da173716f4a2fc3892772986cd4
lmnetsoft <lmnetsoft@gmail.com>
2021-11-02 18:53:47 +0700

    Update jenkins-init.sh

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -4,4 +4,4 @@
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo "deb http://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
 apt-get update
-apt-get install -y jenkins=2.46.3
+apt-get install -y jenkins=2.32.3
